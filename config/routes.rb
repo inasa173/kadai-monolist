e226b6fb646c329f362d4e108d53a634c6a9c263
Hitoshi Inasa <hitoshi-inasa@dac.co.jp>
2017-06-23 05:11:01 +0000

    ranking

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
+
+  get 'rankings/want', to: 'rankings#want'
+  get 'rankings/have', to: 'rankings#have'
+
   resources :users, only: [:show, :create, :new]
 
   resources :items, only: [:show, :new]
